b953dc11972bc4e5cab86300a72aa06ab0752c2b
therandomfool <mddenver@protonmail.com>
2020-11-22 16:26:59 -0800

    added README and finished JS

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Full page JS slider
 
+# JS
+- document
+- .querySelector()
+- .getElementsByTagName()
+- .classList.add() / .classList.remove() to move through slider text and image.
+
+# CSS
+- :before / :hover Pseudo Class
+- :nth-child(n)
+- transform
 
 
-![fullpageslider](img/uc.gif)
+
+![fullpageslider](img/slider.gif)
--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const imgBx = document.querySelector('.imgBx')
 const slides = imgBx.getElementsByTagName('img');
-let i = 0;
+let j= 0;
 
 function nextSlide(){
     slides[i].classList.remove('active');
@@ -14,3 +14,22 @@ function prevSlide(){
     i = (i - 1 + slides.length) % slides.length;
     slides[i].classList.add('active')
 }
+
+
+const contentBx = document.querySelector('.contentBx')
+const slidesText = contentBx.getElementsByTagName('div');
+let i = 0;
+
+function nextSlideText(){
+    slidesText[j].classList.remove('active');
+    j = (j + 1) % slidesText.length;
+    slidesText[j].classList.add('active')
+}
+function prevSlideText(){
+    slidesText[j].classList.remove('active');
+    j = (j - 1 + slidesText.length) % slidesText.length;
+    slidesText[j].classList.add('active')
+}
+
+
+
